2c07648ef5a5ca12db116025f99db7ca06cb2380
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-07-29 14:25:44 -0300

    chore: add mocks file and images

---
+++ src/mocks/banda.js
@@ -0,0 +1,38 @@
+export const bandas = [
+    {
+      titulo: "Alma Negra",
+      descricao:
+        "Corpo em Mogno, acabamento do corpo em Níquel, Braço em Mahogany, chave seletora 3 posições, ponte LockTone ABR Tune-o-matic e captadore do braço Epiphone ProBucker-2 e captação da ponte Seymour Duncan ProModel DimeBag Darrell",
+      foto: require("assets/imagens/almanegra.jpg").default,      
+    },
+    {
+      titulo: "Sem futuro",
+      descricao:
+        "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+      foto: require("assets/imagens/semfuturo.jpg").default,      
+    },
+    {
+      titulo: "Tripanossoma",
+      descricao:
+        "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+      foto: require("assets/imagens/tripa.jpg").default,      
+    },
+    {
+      titulo: "U.R.S.O",
+      descricao:
+        "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+      foto: require("assets/images/urso.jpg").default,      
+    },
+    {
+      titulo: "Ferida Aberta",
+      descricao:
+        "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+      foto: require("assets/imagens/ferida.jpg").default,      
+    },
+    {
+      titulo: "Metal Pesado",
+      descricao:
+        "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+      foto: require("assets/imagens/slammer.jpg").default,      
+    },
+  ];
