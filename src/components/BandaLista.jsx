c334f6f7d9ef110827a03546e0273c953fbed4b3
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-08-01 16:41:21 -0300

    chore: add conditional style

--- src/components/BandaLista.jsx
+++ src/components/BandaLista.jsx
@@ -11,12 +11,26 @@ function BandaLista() {
     setBandaSelecionada({ ...bandaSelecionada, ...banda }); //passa a paletaselecionada  e acrescenta o click, apos é só criar a config do botao abaixo e depois criar o span para modificar o valor dos clicks
   };
 
+  const removerItem = (bandaIndex) => {
+    const banda = {
+      [bandaIndex]: Number(bandaSelecionada[bandaIndex] || 0) - 1,
+    };
+    setBandaSelecionada({ ...bandaSelecionada, ...banda });
+  };
+
   // possue dois parametros a 1° a condição e 2° a index da banda selecionada
   //se o canRender for true ele renderiza o span senao não
-
   const badgeCounter = (canRender, index) =>
-	Boolean(canRender) && (<span className="BandaListaItem__badge"> {bandaSelecionada[index]} </span>);
+    Boolean(canRender) && (
+      <span className="BandaListaItem__badge"> {bandaSelecionada[index]} </span>
+    );
 
+  const removeButton = (canRender, index) =>
+    Boolean(canRender) && (
+      <button className="Acoes__remover" onClick={() => removerItem(index)}>
+        remover
+      </button>
+    );
 
   return (
     <div className="BandaLista">
@@ -27,13 +41,15 @@ function BandaLista() {
             <div className="BandaListaItem__titulo">{banda.titulo}</div>
             <div className="BandaListaItem__descricao">{banda.descricao}</div>
             <div className="BandaListaItem__acoes Acoes">
-              
               <button
-                className="Acoes__adicionar Acoes__adicionar--preencher"
-                onClick={() => adicionarItem(index)}//add o index da paleta e ativa a função paraadd item passando o index da banda selecionada.
+                className={`Acoes__adicionar ${
+                  !bandaSelecionada[index] && "Acoes__adicionar--preencher"
+                }`} //mudar o tamanho do botao com click
+                onClick={() => adicionarItem(index)} //add o index da paleta e ativa a função paraadd item passando o index da banda selecionada.
               >
                 adicionar
               </button>
+              {removeButton(bandaSelecionada[index], index)}
             </div>
           </div>
           <img className="BandaListaItem__foto" src={banda.foto} alt="" />
